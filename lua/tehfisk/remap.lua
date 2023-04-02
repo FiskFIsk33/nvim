0b0f52ab055ca83615419ed3e20404e76b47bc72
Jonathan Johnson <git@tehfisk.se>
2023-04-02 06:45:13 +0200

    vim automatic git

--- lua/tehfisk/remap.lua
+++ lua/tehfisk/remap.lua
@@ -10,7 +10,7 @@ vim.keymap.set("n", "<C-P>", ":FZF<CR>")
 --keep centered when moving around
 vim.keymap.set("n", "<C-D>", "<C-D>zz")
 vim.keymap.set("n", "<C-U>", "<C-U>zz")
-vim.keymap.set("n", "n", "nzz")
-vim.keymap.set("n", "N", "Nzz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
 
 
