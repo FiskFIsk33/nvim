781adf366b6880b48a2f4099e5e5983638aa1ee7
Jonathan Johnson <git@tehfisk.se>
2023-05-11 08:34:58 +0200

    vim automatic git

--- lua/tehfisk/remap.lua
+++ lua/tehfisk/remap.lua
@@ -5,13 +5,3 @@ vim.keymap.set("n", "<C-K>", "<C-W><C-K>")
 vim.keymap.set("n", "<C-L>", "<C-W><C-L>")
 vim.keymap.set("n", "<C-H>", "<C-W><C-H>")
 
-vim.keymap.set("n", "<C-P>", ":FZF<CR>")
-
---keep centered when moving around
-vim.keymap.set("n", "<C-D>", "<C-D>zz")
-vim.keymap.set("n", "<C-U>", "<C-U>zz")
-vim.keymap.set("n", "n", "nzzzv")
-vim.keymap.set("n", "N", "Nzzzv")
-
---vim fugitive
-vim.keymap.set("n", "<leader>gs", vim.cmg.Git)
