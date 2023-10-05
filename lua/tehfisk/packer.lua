73518f6d88a5e947838c0068d6c649d2e2202bc5
FiskFisk33 <github@tehfisk.se>
2023-10-05 07:48:01 +0200

    vim automatic git

--- lua/tehfisk/packer.lua
+++ lua/tehfisk/packer.lua
@@ -12,6 +12,7 @@ return require('packer').startup(function(use)
 	use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
 
 	use 'junegunn/fzf.vim'
+	asdfdsaf
 	--use 'mrjones2014/nvim-ts-rainbow'
 	--use 'hiphish/rainbow-delimiters.nvim'
 	use 'lambdalisue/suda.vim'
