292d8abb742fd2f26c4fced9433cbe8ac456cb24
FiskFisk33 <github@tehfisk.se>
2023-10-06 22:37:56 +0200

    set up autocomplete

--- lua/tehfisk/cmp.lua
+++ lua/tehfisk/cmp.lua
@@ -2,6 +2,10 @@ local cmp = require('cmp')
 local cmp_action = require('lsp-zero').cmp_action()
 
 cmp.setup({
+  preselect = 'item',
+  completion = {
+    completeopt = 'menu,menuone,noinsert'
+  },
   sources = {
     {name = 'path'},
     {name = 'nvim_lsp'},
@@ -9,7 +13,30 @@ cmp.setup({
     {name = 'luasnip', keyword_length = 2},
   },
   mapping = {
-    ['<C-f>'] = cmp_action.luasnip_jump_forward(),
-    ['<C-b>'] = cmp_action.luasnip_jump_backward(),
-  }
+    ['<C-y>'] = cmp.mapping.confirm({select = false}),
+    ['<C-e>'] = cmp.mapping.abort(),
+    ['<Up>'] = cmp.mapping.select_prev_item({behavior = 'select'}),
+    ['<Down>'] = cmp.mapping.select_next_item({behavior = 'select'}),
+    ['<C-p>'] = cmp.mapping(function()
+      if cmp.visible() then
+        cmp.select_prev_item({behavior = 'select'})
+      else
+        cmp.complete()
+      end
+    end),
+    ['<C-n>'] = cmp.mapping(function()
+      if cmp.visible() then
+        cmp.select_next_item({behavior = 'select'})
+      else
+        cmp.complete()
+      end
+    end),
+    ['<Tab>'] = cmp_action.luasnip_supertab(),
+    ['<S-Tab>'] = cmp_action.luasnip_shift_supertab(),
+    --
+    -- Scroll up and down in the completion documentation
+    ['<C-f>'] = cmp.mapping.scroll_docs(-2),
+    ['<C-b>'] = cmp.mapping.scroll_docs(2),
+  },
+
 })
