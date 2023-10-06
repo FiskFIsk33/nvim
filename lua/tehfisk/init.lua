a224f99136a470c5eadf83ac1c08687c9b91f769
FiskFisk33 <github@tehfisk.se>
2023-10-06 06:49:09 +0200

    vim automatic git

--- lua/tehfisk/init.lua
+++ lua/tehfisk/init.lua
@@ -7,6 +7,7 @@ require("tehfisk.remap")
 require("tehfisk.vimtex")
 require("tehfisk.autocmd")
 require("tehfisk.lsp")
+require("tehfisk.vimtex")
 
 vim.opt.encoding = 'utf-8'
 vim.opt.clipboard = 'unnamedplus'
