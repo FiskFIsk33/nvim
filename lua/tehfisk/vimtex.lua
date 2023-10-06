d904df10a4699e12fa872007e6d8dd36151e142f
FiskFisk33 <github@tehfisk.se>
2023-10-06 07:00:50 +0200

    vim automatic git

--- lua/tehfisk/vimtex.lua
+++ lua/tehfisk/vimtex.lua
@@ -4,3 +4,5 @@ vim.g.vimtex_view_forward_search_on_start = false
 vim.g.vimtex_compiler_method = 'latexmk'
 vim.g.tex_flavor = 'latex'
 
+--vim.g.vimtex_compiler_latexmk = {'aux-dir': {-> expand("%:t:r")}}
+vim.g.vimtex_compiler_latexmk = {'aux-dir': {-> expand("%:t:r")}}
