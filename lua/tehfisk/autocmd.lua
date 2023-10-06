f7903ea8ec8fdd0071f818ae750e0d421fa8fd8a
FiskFisk33 <github@tehfisk.se>
2023-10-06 16:11:12 +0200

    vim automatic git

--- lua/tehfisk/autocmd.lua
+++ lua/tehfisk/autocmd.lua
@@ -3,9 +3,9 @@ vim.cmd [[
     autocmd BufWritePost ~/.dotfiles/* :!git -C %:p:h add . && git -C %:p:h commit -a -m "vim automatic git" && git -C %:p:h push origin master && echo %:p 
 ]]
 --auto push changes in ~/.config/nvim to github
-vim.cmd [[
-    autocmd BufWritePost ~/.config/nvim/* :!git -C %:p:h add . && git -C %:p:h commit -a -m "vim automatic git" && git -C %:p:h push origin master && echo %:p 
-]]
+--vim.cmd [[
+--    autocmd BufWritePost ~/.config/nvim/* :!git -C %:p:h add . && git -C %:p:h commit -a -m "vim automatic git" && git -C %:p:h push origin master && echo %:p 
+--]]
 
 --when opening latex file
 vim.api.nvim_create_autocmd({"BufEnter", "BufWinEnter"}, {
