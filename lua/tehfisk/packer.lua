1f5fc022c98f0977f46c607c3fdc795371ac4a16
FiskFisk33 <github@tehfisk.se>
2023-10-08 14:02:12 +0200

    added telescope

--- lua/tehfisk/packer.lua
+++ lua/tehfisk/packer.lua
@@ -31,6 +31,11 @@ return require('packer').startup(function(use)
 			{ 'L3MON4D3/LuaSnip' },
 		}
 	}
+	use {
+	  'nvim-telescope/telescope.nvim', tag = '0.1.3',
+	-- or                            , branch = '0.1.x',
+	  requires = { {'nvim-lua/plenary.nvim'} }
+	}
 	use 'theprimeagen/vim-be-good'
 	use 'mbbill/undotree'
 	use 'tpope/vim-fugitive'
