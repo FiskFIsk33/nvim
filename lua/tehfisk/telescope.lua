63dd2c20730720600e9f5f4f9bb5e4e3c3c87ddb
FiskFisk33 <github@tehfisk.se>
2023-10-11 08:36:45 +0200

    added command history shortcut

--- lua/tehfisk/telescope.lua
+++ lua/tehfisk/telescope.lua
@@ -3,6 +3,7 @@ vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+vim.keymap.set('n', '<leader>fc', builtin.command_history, {})
 
 require('telescope').setup({
 	defaults = {
@@ -25,26 +26,13 @@ require('telescope').setup({
 		find_files = {
 			hidden = true
 		},
-	},
-	extensions = {
-		["ui-select"] = {
-			require("telescope.themes").get_dropdown {
-				-- even more opts
+		command_history = {
+			layout_config = {
+				horizontal = {
+					width = 50,
+					height = 40
+				}
 			}
-
-			-- pseudo code / specification for writing custom displays, like the one
-			-- for "codeactions"
-			-- specific_opts = {
-			--   [kind] = {
-			--     make_indexed = function(items) -> indexed_items, width,
-			--     make_displayer = function(widths) -> displayer
-			--     make_display = function(displayer) -> function(e)
-			--     make_ordinal = function(e) -> string
-			--   },
-			--   -- for example to disable the custom builtin "codeactions" display
-			--      do the following
-			--   codeactions = false,
-			-- }
 		}
 	},
 	vim.api.nvim_set_hl(0, 'TelescopeBorder', { fg = "#555555" }),
