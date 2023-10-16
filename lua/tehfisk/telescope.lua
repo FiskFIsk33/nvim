b79dd0aaaa6514fc70d07875798af544bf5b8523
FiskFisk33 <github@tehfisk.se>
2023-10-16 09:07:08 +0200

    fixed fzf config err

--- lua/tehfisk/telescope.lua
+++ lua/tehfisk/telescope.lua
@@ -46,6 +46,15 @@ require('telescope').setup({
 			},
 		}
 	},
+	extensions = {
+		fzf = {
+			fuzzy = true,                    -- false will only do exact matching
+			override_generic_sorter = true,  -- override the generic sorter
+			override_file_sorter = true,     -- override the file sorter
+			case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
+			-- the default case_mode is "smart_case"
+		}
+	},
 	vim.api.nvim_set_hl(0, 'TelescopeBorder', { link = "GruvboxGray" }),
 	vim.api.nvim_set_hl(0, 'TelescopePromptBorder', { link = "GruvboxGray" }),
 	vim.api.nvim_set_hl(0, 'TelescopeResultsBorder', { link = "GruvboxGray" }),
