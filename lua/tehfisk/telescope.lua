4faf6be9957f499692cdcea0504e8e0263322a0d
FiskFisk33 <github@tehfisk.se>
2024-02-22 07:22:17 +0100

    changed to standard live grep, the old one was a bit broken

--- lua/tehfisk/telescope.lua
+++ lua/tehfisk/telescope.lua
@@ -27,7 +27,7 @@ local function live_grep_from_project_git_root()
 end
 
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', live_grep_from_project_git_root, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 vim.keymap.set('n', '<leader>fc', builtin.command_history, {})
